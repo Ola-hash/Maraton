747085974a2229327674df8333d10d21c134c689
Ola <ola.nauka@gmail.com>
2020-10-22 12:01:44 +0200

    Dodanie metody przeliczającej sekundy na minuty w klasie Maraton.

--- src/Maraton.java
+++ src/Maraton.java
@@ -1,7 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Maraton {
@@ -31,21 +30,36 @@ public class Maraton {
         }
     }
 
-    public void najszybszyUczestnik() {
-        Integer najszybczyCzas = null;
-        for (Integer czasUczestnika : czas) {
-            najszybczyCzas = Collections.min(czas);
-        }
-        System.out.println("Najszybszy uczestnik maratonu uzyskał czas "+ najszybczyCzas+" sekund.");
+    public String sekundyNaGodziny(int wartosc) {
+        String wynik = "";
+        int liczbaSekund = wartosc % 60;
+        int liczbaMinut = wartosc / 60;
+        return wynik += liczbaMinut + " minut i " + liczbaSekund + " sekund.";
+
     }
 
-    public void najwolniejszyUczestnik() {
-        Integer najwolniejszyCzas = null;
-        for (Integer czasUczestnika : czas) {
-            najwolniejszyCzas = Collections.max(czas);
-        }
-        System.out.println("Najwolniejszy uczestnik maratonu uzyskal czas "+najwolniejszyCzas+ " sekund.");
+    public void najszybszyUczestnik() {
+        int min = Integer.MAX_VALUE;
+        String wartosc = null;
+        for (int i = 0; i < czas.size(); i++) {
+            if (czas.get(i) < min) {
 
+                wartosc = sekundyNaGodziny(min = czas.get(i));
+            }
+        }
+        System.out.println("Najszybszy uczestnik maratonu uzyskał czas " + wartosc);
+    }
+
+
+    public void najwolniejszyUczestnik() {
+        String wartosc = null;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < czas.size(); i++) {
+            if (czas.get(i) > max) {
+                wartosc = sekundyNaGodziny(max = czas.get(i));
+            }
+        }
+        System.out.println("Najwolniejszy uczestnik maratonu uzyskal czas " + wartosc);
     }
 
 }
